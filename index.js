4ea447cf9f558542a832b81166c6c273f77e7a5c
habeeb murtala <murtalahabeeb@gmail.com>
2019-12-22 23:19:40 +0100

    modernblog

---
+++ index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	
+	$(".b1").click(function(){
+		$(".hi").slideToggle(1000);
+	});
+
+	$(".b2").click(function(){
+		$(".hi2").slideToggle(1000);
+	});
+
+	$(".b3").click(function(){
+		$(".hi3").slideToggle(1000);
+	});
+
+	$(".b4").click(function(){
+		$(".hi4").slideToggle(1000);
+	});
+
+	$(".b5").click(function(){
+		$(".hi5").slideToggle(1000);
+	});
+
+	$(".b6").click(function(){
+		$(".hi6").slideToggle(1000);
+	});
+
+	$(".b7").click(function(){
+		$(".hi7").slideToggle(1000);
+	});
+
+	$(".b8").click(function(){
+		$(".hi8").slideToggle(1000);
+	});
+
+	$(".b9").click(function(){
+		$(".hi9").slideToggle(1000);
+	});
+
+	$(".b10").click(function(){
+		$(".hi10").slideToggle(1000);
+	});
+
+	$(".b11").click(function(){
+		$(".hi11").slideToggle(1000);
+	});
+
+	$(".b12").click(function(){
+		$(".hi12").slideToggle(1000);
+	});
+});
